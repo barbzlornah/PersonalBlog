722be54e18a8252e6a30a0e9a2e903951c19fc49
Barbara Masinde <barbzlornah@gmail.com>
2020-10-08 20:08:24 +0300

    new commit

--- sanity-react/src/App.js
+++ sanity-react/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import AllPosts from "./components/AllPosts.js";
+import OnePost from "./components/OnePost.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Route component={AllPosts} path="/" exact />
+        <Route component={OnePost} path="/:slug" />
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ sanity-react/src/components/OnePost.js
@@ -0,0 +1,7 @@
+// src/components/OnePost.js
+
+import React from "react";
+
+export default function OnePost() {
+  return <h2>OnePost Page</h2>;
+}
